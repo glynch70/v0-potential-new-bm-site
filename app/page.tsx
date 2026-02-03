f38ac9ca6e077050ccf0df511a4f994bd7d3523e
glynch70 <garry.lynch70@gmail.com>
2026-02-03 14:23:06 +0000

    "Increase mobile bottom padding for navbar visibility"

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ export default function BearMediaWebsite() {
   }
 
   return (
-    <div className="pb-24 sm:pb-0">
+    <div className="pb-32 sm:pb-0">
       {/* WebGL Shader Hero */}
       <BearMediaHero />
 
